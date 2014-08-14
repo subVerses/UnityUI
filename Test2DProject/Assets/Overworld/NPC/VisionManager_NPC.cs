e32a211648b0eecca31c55e09584b10026fc60ee
mbuciuman <mbuciuma@gmail.com>
2014-08-13 23:54:06 -0400

    Working Detection but not Displaying Vision

--- Test2DProject/Assets/Overworld/NPC/VisionManager_NPC.cs
+++ Test2DProject/Assets/Overworld/NPC/VisionManager_NPC.cs
@@ -3,24 +3,24 @@ using System.Collections;
 
 public class VisionManager_NPC : MonoBehaviour {
 	
-	GameObject npcSprite; //testing purposes
+	BoxCollider2D npcCollider; //testing purposes
 	NPC_Interactable npcDir;
 	GameObject charController;
 	SpriteMover charDir;
-	GameObject SneakIcon;
 	SneakBar sneakBar;
+	bool isSeen;
 
 	public Ray visionRay;
 	public int currentDirection;
 	public Vector3 dirVector;
 	
 	void Start () {
-		npcSprite = GameObject.Find ("TestNPCSprite");
-		npcDir = npcSprite.GetComponent<NPC_Interactable>();
-		charController = GameObject.Find ("CharacterController");
+		npcCollider = transform.GetComponentInChildren<BoxCollider2D>();
+		npcDir = npcCollider.GetComponent<NPC_Interactable>();
+		charController = GameObject.Find ("SpriteController");
 		charDir = charController.GetComponent<SpriteMover>();
-		SneakIcon = GameObject.Find ("SneakIcon");
-		sneakBar = SneakIcon.GetComponent<SneakBar>();
+		sneakBar = new SneakBar();
+		isSeen = false;
 
 		currentDirection = 0;
 		visionRay = new Ray();
@@ -30,7 +30,7 @@ public class VisionManager_NPC : MonoBehaviour {
 	//gets the direction the npc is facing, then checks the transform matrix of that npc, then fires a ray in the direction the npc is facing
 	void Update () {
 		npcDir.setCurrentDirection (3);
-		currentDirection = npcDir.getCurrentDirection();
+		currentDirection = 3;
 
 		switch(currentDirection){
 			case -1:
@@ -48,24 +48,42 @@ public class VisionManager_NPC : MonoBehaviour {
 				dirVector = new Vector3(0, -1, 0);
 				break;
 		}
-
-		visionRay = new Ray (npcSprite.transform.localPosition, dirVector);
+		
+		visionRay = new Ray (new Vector3(npcCollider.bounds.center.x, npcCollider.bounds.center.y), dirVector);
 		Debug.Log ("THE RAY HAS BEEN DRAWN");
 
+	}
+	
+	void OnGUI() {
 		if(isPlayerSeen())
 		{
-			sneakBar.setDetected(true);
+			sneakBar.incrementDetection();
 		}
 		else
 		{
-			sneakBar.setDetected(false);
+			sneakBar.decrementDetection();
 		}
+		bool enabledBackup = GUI.enabled;
+		int detection = sneakBar.getDetection;
+		if(detection == 0){
+			GUI.Box(new Rect(0, 0, Screen.width/5f, Screen.height/5f), sneakBar.visibilityStage0);}
+		else if (detection == 1){
+			GUI.Box(new Rect(0, 0, Screen.width/5f, Screen.height/5f), sneakBar.visibilityStage1);}
+		else if (detection == 2){
+			GUI.Box(new Rect(0, 0, Screen.width/5f, Screen.height/5f), sneakBar.visibilityStage2);}
+		else if (detection == 3){
+			isSeen = true;
+			GUI.Box(new Rect(0, 0, Screen.width/5f, Screen.height/5f), sneakBar.visibilityStage3);}
+		else if (detection == 4){
+			isSeen = true;
+			GUI.Box(new Rect(0, 0, Screen.width/5f, Screen.height/5f), sneakBar.visibilityStage4A);}
+		GUI.enabled = enabledBackup;
 	}
 
 	public bool isPlayerSeen() //checks the bounds of the character compared to the ray of the vision
 	{
 		Bounds b = charDir.getBounds ();
-		return b.IntersectRay(visionRay);
+		Debug.Log (b.IntersectRay (visionRay));
+		return !b.IntersectRay(visionRay);
 	}
-
 }
--- Test2DProject/Assets/SneakMeter/SneakBar.cs
+++ Test2DProject/Assets/SneakMeter/SneakBar.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SneakBar : MonoBehaviour {
+public class SneakBar {
 
 	//public SneakBarController SneakBar;
 	public bool isDetected; //during detection, eye widens
@@ -18,53 +18,28 @@ public class SneakBar : MonoBehaviour {
 	private static int MAX_DETECTED = 4; //maximum value of detectionticks needed for isSeen
 
 	// Use this for initialization
-	void Start () {
+	public SneakBar () {
 		isDetected = false;
 		detectionTicks = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(IsDetected)
-		{
-			detectionTicks++;
-		}
-		else
-		{
-			detectionTicks--;
-		}
+
+	public void incrementDetection() {
+		detectionTicks++;
 	}
 
-	void OnGUI() {
-		bool enabledBackup = GUI.enabled;
-		if(detectionTicks == 0){
-			GUI.Box(new Rect(Screen.width, Screen.height, Screen.width/5f, Screen.height/5f), visibilityStage0);}
-		else if (detectionTicks == 1){
-			GUI.Box(new Rect(Screen.width, Screen.height, Screen.width/5f, Screen.height/5f), visibilityStage1);}
-		else if (detectionTicks == 2){
-			GUI.Box(new Rect(Screen.width, Screen.height, Screen.width/5f, Screen.height/5f), visibilityStage2);}
-		else if (detectionTicks == 3){
-			isSeen = true;
-			GUI.Box(new Rect(Screen.width, Screen.height, Screen.width/5f, Screen.height/5f), visibilityStage3);}
-		else if (detectionTicks == 4){
-			isSeen = true;
-			GUI.Box(new Rect(Screen.width, Screen.height, Screen.width/5f, Screen.height/5f), visibilityStage4A);}
-		GUI.enabled = enabledBackup;
+	public void decrementDetection() {
+		detectionTicks--;
 	}
-	
-	public void setDetected (bool val){
-		isDetected = val;
+
+	public int getDetection {
+		get {
+			return detectionTicks;
+		}
 	}
 
 	public bool NpcIsHostile {
 		get {
 			return npcIsHostile;
-		}
-	}
-
-	public bool IsDetected {
-		get {
-			return isDetected;
 		}
 	}
 }
